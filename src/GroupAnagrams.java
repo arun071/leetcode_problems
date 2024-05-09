a3d0aa58fd91c436d715774f5d8701ef530f9113
arun071 <arun07160@gmail.com>
2024-05-09 20:59:39 +0530

    09-05

---
+++ src/GroupAnagrams.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class GroupAnagrams {
+    public static List<List<String>> groupAnagrams(String[] strs) {
+        if (strs == null || strs.length == 0) {
+            return new ArrayList<>();
+        }
+        Map<String, List<String>> map = new HashMap<>();
+        for (String str : strs) {
+            char[] charArray = str.toCharArray();
+            Arrays.sort(charArray);
+            String sortedStr = String.valueOf(charArray);
+            if (!map.containsKey(sortedStr)) {
+                map.put(sortedStr, new ArrayList<>());
+            }
+            map.get(sortedStr).add(str);
+        }
+        return new ArrayList<>(map.values());
+    }
+
+    public static void main(String[] args) {
+        String arr[] = {"cat", "bat", "eat", "ate"};
+        System.out.println(groupAnagrams(arr));
+    }
+}
+//49.Group Anagrams
+//int arr[] = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89,97, 101};
