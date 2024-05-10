b471f199282a353523032ff29094c2b336216137
arun071 <arun07160@gmail.com>
2024-05-11 01:31:38 +0530

    10/05

---
+++ src/PlusOne.java
@@ -0,0 +1,24 @@
+import java.util.Arrays;
+import java.util.SortedMap;
+
+public class PlusOne {
+    public static int[] plusOne(int[] digits) {
+        for (int i = digits.length-1; i >= 0; i--) {
+            if (digits[i] + 1 < 10) {
+                digits[i]++;
+                return digits;
+            } else {
+                digits[i] = 0;
+            }
+        }
+        digits = new int[digits.length + 1];
+        digits[0] = 1;
+        return digits;
+
+    }
+
+    public static void main(String[] args) {
+        int[] digits = {9,9,9,9};
+        System.out.println(Arrays.toString(plusOne(digits)));
+    }
+}
