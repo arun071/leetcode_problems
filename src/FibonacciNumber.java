426dcedec20b61562bb7a2e07d40682a7bea17ca
arun071 <arun07160@gmail.com>
2024-06-23 13:02:43 +0530

    23/06

---
+++ src/FibonacciNumber.java
@@ -0,0 +1,25 @@
+import java.util.Arrays;
+
+public class FibonacciNumber {
+    public static int recfib(int n) {
+        if (n < 2)
+            return n;
+        return fib(n - 1) + fib(n - 2);
+    }
+
+    public static int fib(int n) {
+
+        if (n < 2)
+            return n;
+
+        int[] arr = new int[n + 1];
+        arr[0] = 0;
+        arr[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            arr[i] = arr[i - 1] + arr[i - 2];
+        }
+        return arr[n];
+    }
+}
+
+
